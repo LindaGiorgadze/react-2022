7a19aa4b1f886f4a254dfd010e9b7ac60e665c58
Linda Giorgadze <linda.giorgadze17b@geolab.edu.ge>
2022-07-14 21:36:34 +0400

    Added Swiper and Intersection Observer

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,12 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { InView } from "react-intersection-observer";
 
 export default function Products() {
   const [fetchedData, setFetchedData] = useState([]);
@@ -14,13 +20,48 @@ export default function Products() {
   return (
     <section>
       <h3>Products</h3>
+      <InView
+        threshold={0.8}
+        rootMargin='20px 0px'
+      >
+        {({ inView, ref, entry }) => (
+          <div ref={ref}>
+            <h2>{`Header inside viewport ${inView}.`}</h2>
+            <div className={`box ${inView ? 'boxAnimation' : ''}`}></div>
+          </div>
+        )}
+      </InView>
+      {/* <Swiper
+        spaceBetween={50}
+        slidesPerView={4}
+        loop={true}
+        pagination={{
+          clickable: true
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+      >
+        {fetchedData.map((product) => {
+          return (
+            <SwiperSlide key={product.id}>
+              <div className="ProductBox Slider">
+                <img src={product.img} alt={product.title} />
+                <h4>{product.title}</h4>
+                <Link to={`${product.id}`}>Visit Product</Link>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper> */}
       <div className="grid">
         {fetchedData.map((item) => {
-          return (<div key={item.id} className="ProductBox">
-            <img src={item.img} alt={item.title} />
-            <h4>{item.title}</h4>
-            <Link to={`${item.id}`} >Visit Product</Link>
-            </div>);
+          return (
+            <div key={item.id} className="ProductBox">
+              <img src={item.img} alt={item.title} />
+              <h4>{item.title}</h4>
+              <Link to={`${item.id}`}>Visit Product</Link>
+            </div>
+          );
         })}
       </div>
     </section>
